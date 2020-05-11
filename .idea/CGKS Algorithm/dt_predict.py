816eb9110333f172512494dc4941aa522fd227d9
casporo <casper.g.kaun@gmail.com>
2020-05-11 15:47:40 +0800

    Rearranged test data to be used                                                                             Generated new set of test data for testing and revamped certain parts of the code

--- .idea/CGKS Algorithm/dt_predict.py
+++ .idea/CGKS Algorithm/dt_predict.py
@@ -10,16 +10,13 @@ from sklearn.model_selection import train_test_split
 #Import the dataset
 #df = pandas.read_csv(r"data_files\TRAINING_DATA_DATASET_A.csv")
 df = pandas.read_csv(r"data_files\TRAINING_DATA_DATASET_B.csv")
-#df = pandas.read_csv(r"data_files\RANDOM_DATA_DATASET_A.csv")
-#df = pandas.read_csv(r"data_files\RANDOM_DATA_DATASET_B.csv")
-print(df)
+#dt = pandas.read_csv(r"data_files\RANDOM_DATA_DATASET_B.csv")
 
 #To split the dataset into features and target variable
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
-
-#Split the dataset into 80% training data and 20% of testing data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 1)
+X_train = df.iloc[:, :-1].values
+y_train = df.iloc[:, -1].values
+X_test = dt.iloc[:, :-1].values
+y_test = dt.iloc[:, -1].values
 
 #train the model with the help of DecisionTreeClassifier class of sklearn
 dtree = DecisionTreeClassifier()
@@ -27,17 +24,20 @@ dtree = dtree.fit(X_train,y_train)
 
 #To make a prediction
 y_pred = dtree.predict(X_test)
-#np.set_printoptions(threshold=sys.maxsize)
-#print(y_pred)
-#print(y_test)
+print(y_pred)
+np.set_printoptions(threshold=sys.maxsize)
 
+print("[2] means 'High'")
+print("[1] means 'Low'")
+
+"""
 #To obtain the accuracy score, confusion matrix and classification report
-result = confusion_matrix(y_test, y_pred)
+result = confusion_matrix(y_train, y_pred)
 print("Confusion Matrix:")
 print(result)
-result1 = classification_report(y_test, y_pred)
+result1 = classification_report(y_train, y_pred)
 print("Classification Report:",)
 print (result1)
-result2 = accuracy_score(y_test,y_pred)
+result2 = accuracy_score(y_train,y_pred)
 print("Accuracy:",result2)
-
+"""
--- .idea/CGKS Algorithm/rf_predict.py
+++ .idea/CGKS Algorithm/rf_predict.py
@@ -1,26 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 #dataset = pd.read_csv(r"data_files\TRAINING_DATA_DATASET_A.csv")
-#dataset = pd.read_csv(r"data_files\TRAINING_DATA_DATASET_B.csv")
-dataset = pd.read_csv(r"data_files\RANDOM_DATA_DATASET_A.csv")
-#dataset = pd.read_csv(r"data_files\RANDOM_DATA_DATASET_B.csv")
+dataset = pd.read_csv(r"data_files\TRAINING_DATA_DATASET_B.csv")
+testset = pd.read_csv(r"data_files\RANDOM_DATA_DATASET_B.csv")
 dataset.head()
 print(dataset)
 
 #Data Preprocessing
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-#print(X)
-#print(y)
-
-#divide the data into train and test split. The following code will split the dataset into 80% training data and 20% of testing data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+X_train = dataset.iloc[:, :-1].values
+y_train = dataset.iloc[:, -1].values
+X_test = testset.iloc[:, :-1].values
+y_test = testset.iloc[:, -1].values
 
 #train the model with the help of RandomForestClassifier class of sklearn
 classifier = RandomForestClassifier(n_estimators = 100)
@@ -28,13 +25,20 @@ classifier.fit(X_train, y_train)
 
 ##To make a prediction
 y_pred = classifier.predict(X_test)
+np.set_printoptions(threshold=sys.maxsize)
+print(y_pred)
 
+print("[2] means 'High'")
+print("[1] means 'Low'")
+
+"""
 #To obtain the accuracy score, confusion matrix and classification report
-result = confusion_matrix(y_test, y_pred)
+result = confusion_matrix(y_train, y_pred)
 print("Confusion Matrix:")
 print(result)
-result1 = classification_report(y_test, y_pred)
+result1 = classification_report(y_train, y_pred)
 print("Classification Report:",)
 print (result1)
-result2 = accuracy_score(y_test,y_pred)
+result2 = accuracy_score(y_train,y_pred)
 print("Accuracy:",result2)
+"""
